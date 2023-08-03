80ff13cd55ed1da773d16cbf5e6d7f3fe92479a0
Tomé Luan <tomeluan@MacBook-Pro-de-Tome.local>
2023-08-02 22:42:24 -0300

    finalizando a calculadora por hora

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,11 +16,14 @@ calcular.addEventListener('click', (e)=>{
     let valor_juros;
     let valor_liquido = valor_produto.value - valor_entrada.value;
 
-    if(valor_produto.value <= valor_entrada.value){
+    if(valor_produto.value == '' || valor_entrada.value == ''){
+        alert('Por favor, verifique se os campos estão preenchidos corretamente!')
+    }
+    else if(valor_produto.value <= valor_entrada.value){
         alert('O valor de entrada não pode ser maior ou igual ao valor do produto!')
     }
+    else {
 
-    else{
         if(valorSelecionado == 1){
             valor_juros = (valor_liquido * 0.05);
         }
@@ -77,8 +80,8 @@ calcular.addEventListener('click', (e)=>{
         let valor_da_parcela = (valor_total / valorSelecionado).toFixed(2);
     
         resultado.innerHTML = `
-            <p>O valor do seu produto no cartão de crédito, é de R$${valor_total} reais.
-            Este valor será dividido em ${valorSelecionado} parcela(s) de R$${valor_da_parcela} reais</p>
+            <p>O valor do seu produto no cartão de crédito, é de R$${valor_total}.
+            Este valor será dividido em ${valorSelecionado} parcela(s) de R$${valor_da_parcela}.</p>
         `
     }
 })
